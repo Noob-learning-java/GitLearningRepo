b97baadfd5246ed5c617bb7e7460c48c928ab5f3
Your Name <you@example.com>
2020-05-24 19:35:39 +0530

    initial commit

---
+++ Hello_World.java
@@ -0,0 +1,13 @@
+package beginer;
+import java.util.Scanner;
+public class Hello_World {
+
+	public static void main(String[] args) {
+		int i;
+		Scanner s=new Scanner(System.in);
+		i=s.nextInt();
+		System.out.println("Hello World\t"+i+"\nSreejit Ghosh");
+
+	}
+
+}
